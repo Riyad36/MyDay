68fb3acce77660a2a046716189e7285171157ae0
Riyad36 <almuhafiz@iut-dhaka.edu>
2020-12-29 11:17:44 +0600

    Initial commit

---
+++ RSS/app/src/main/java/com/example/shadidintisar/rss/Interface/ItemClickListener.java
@@ -0,0 +1,9 @@
+package com.example.shadidintisar.rss.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
+
+
